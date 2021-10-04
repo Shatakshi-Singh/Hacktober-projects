fccad4e6231438621188d9bad3ed563289f0fc2c
Shatakshi Singh <s.shatakshi2103@gmail.com>
2021-10-04 07:11:29 +0530

    init cli

--- cli-quiz/index.js
+++ cli-quiz/index.js
@@ -1,2 +1,52 @@
 var readlineSync = require("readline-sync");
 const chalk = require('chalk');
+
+console.log(chalk.bold.magenta('Hello world!'));
+var userName = readlineSync.question("Hey fren, what's your name? \n");
+console.log(chalk("Welcome! ", userName,". "));
+
+var score = 0;
+
+var highScore = [{
+
+    name: "abc",
+    inScore : 10
+},{
+    name: "xyz",
+    inScore: 7,
+}
+
+]
+var qna = [
+    {
+        question: "Which is my favorite cusine? ",
+        answer: "indian",
+    },
+    {
+        question: "What am I studying right now?",
+        answer: "engineering"
+    },
+]
+function game(question,answer){
+
+    var userAnswer = readlineSync.question(question);
+    if(userAnswer.toUpperCase() === answer.toUpperCase()){
+        console.log("YAY you did it!"),
+        score++;
+        console.log(score);
+    }else{
+        console.log("Opps! Let's hang out soon.")
+        score--;
+        console.log(score);
+    }
+}
+
+for(var i =0; i<qna.length;i++){
+    game(qna[i].question, qna[i].answer);
+}
+console.log("Your final Score is", score);
+
+
+for(var i = 0; i<highScore.length;i++){
+    console.log(highScore[i].name," : ", highScore[i].inScore);
+}
